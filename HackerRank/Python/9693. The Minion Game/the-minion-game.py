a3fbb06a46c7b5792a0b0883ad1d9598ac074d77
Abuzar-amin <133777283+Abuzar-amin@users.noreply.github.com>
2026-08-19 16:33:47 +0530

    [HackerRank][Python][Strings] The Minion Game - Score: 40.0 - AlgorithmHub

---
+++ HackerRank/Python/9693. The Minion Game/the-minion-game.py
@@ -0,0 +1,18 @@
+def minion_game(string):
+    # your code goes here
+    vowels = "AEIOU"
+    kevin = 0
+    stuart = 0
+
+    for i in range(len(string)):
+        if string[i] in vowels:
+            kevin += len(string) - i
+        else:
+            stuart += len(string) - i
+
+    if kevin > stuart:
+        print("Kevin", kevin)
+    elif stuart > kevin:
+        print("Stuart", stuart)
+    else:
+        print("Draw")
